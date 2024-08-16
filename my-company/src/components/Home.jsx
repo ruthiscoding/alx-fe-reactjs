7172f625982ced3e9e7318295e5a6a14d2e5b5d1
Ruth Udoh <ruthudohhappiness@gmail.com>
2024-08-16 21:38:49 +0100

    add files

---
+++ my-company/src/components/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div style={{ display: "flex", padding: "20px", boxSizing: "border-box" }}>
+      <h1>Welcome to Our Company</h1>
+      <p>We are dedicated to delivering excellence in all our services.</p>
+    </div>
+  );
+}
+
+export default Home;
