e0e7b9a26aa8c5c97bf1488294dca71d8fb2190c
Ruth Udoh <ruthudohhappiness@gmail.com>
2024-08-23 21:24:26 +0100

    edit files

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useRecipeStore } from "./recipeStore";
+import { useRecipeStore } from "../recipeStore";
 
 const RecipeList = () => {
   const { filteredRecipes, filterRecipes } = useRecipeStore((state) => ({
--- recipe-sharing-app/src/components/RecommendationsList.jsx
+++ recipe-sharing-app/src/components/RecommendationsList.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { useRecipeStore } from "./recipeStore";
+import { useRecipeStore } from "../recipeStore";
 
 const RecommendationsList = () => {
   const { recipes, recommendations, generateRecommendations } = useRecipeStore(
