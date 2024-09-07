eda6bece9965434c1220b9bc4e5f9fb4ff66a89e
Ruth Udoh <ruthudohhappiness@gmail.com>
2024-09-07 16:11:42 +0100

    edit some files

---
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function UserProfile() {
+  return (
+    <div className="user-profile bg-gray-100 sm:p-4 md:p-8 max-w-xs md:max-w-sm mx-auto my-10 md:my-20 rounded-lg shadow-lg hover:shadow-xl">
+      <img
+        src="https://via.placeholder.com/150"
+        alt="User"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
+      />
+      <h1 className="text-lg md:text-xl text-blue-800 my-4 hover:text-blue-500">
+        John Doe
+      </h1>
+      <p className="text-sm md:text-base text-gray-600">
+        Developer at Example Co. Loves to write code and explore new
+        technologies.
+      </p>
+    </div>
+  );
+}
+
+export default UserProfile;
